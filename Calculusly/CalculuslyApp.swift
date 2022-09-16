80cc14c233e4ff9877a11bbdb47dcdbc3b05741e
AppleJuicyPie <aidenp1123@gmail.com>
2022-09-15 19:53:13 -0700

    Initial Commit

---
+++ Calculusly/CalculuslyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculuslyApp.swift
+//  Calculusly
+//
+//  Created by Aiden Park on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculuslyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
