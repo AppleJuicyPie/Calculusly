4c79151f95beb22858b2d9b2e722286ca1651307
AppleJuicyPie <aidenp1123@gmail.com>
2022-09-15 20:02:44 -0700

    First commit lul

--- Calculusly/ContentView.swift
+++ Calculusly/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Baqua.exe has stopped working")
             .padding()
     }
 }
